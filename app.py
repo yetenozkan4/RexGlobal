8e5840fdaad5b0034b01ba2563941c2d9bf85bbc
yetenozkan4 <yetenozkan4@gmail.com>
2026-05-11 00:55:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -88,4 +88,4 @@ if __name__ == "__main__":
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
