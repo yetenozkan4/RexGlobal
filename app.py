856c0e340248b153541af516dad4e1754caba257
yetenozkan4 <yetenozkan4@gmail.com>
2026-05-11 20:02:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # --- ADMİN AYARLARI ---
 ADMIN_USER = "Administrator" 
-ADMIN_PASS = "AdminRexGlobal" 
+ADMIN_PASS = "adminrex" 
 # ----------------------
 
 DB_PATH = os.path.join(os.path.dirname(__file__), 'database.db')
