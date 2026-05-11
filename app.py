42a0b8eaf9393aebcea4c2fb45f39aa0125513d5
yetenozkan4 <yetenozkan4@gmail.com>
2026-05-11 20:52:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # --- ADMİN AYARLARI ---
 ADMIN_USER = "Administrator" 
-ADMIN_PASS = "adminrex" # <--- Kanka burayı silip kendi şifreni yaz!
+ADMIN_PASS = "GÖRMEMEN İÇİN SİLDİM" # <--- Kanka burayı silip kendi şifreni yaz!
 # ----------------------
 
 DB_PATH = os.path.join(os.path.dirname(__file__), 'database.db')
